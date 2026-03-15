3d1a91ecfd8f087af07f9cdd830486177d6ad387
John <Monyreaksmey.sorm@student.cadt.edu.kh>
2026-03-15 15:58:46 +0700

    Apply gitignore

--- exercice 2/src/App.jsx
+++ exercice 2/src/App.jsx
@@ -21,13 +21,16 @@ const INITIAL_STUFFS = [
 export default function App() {
   const [stuffs, setStuffs] = React.useState(INITIAL_STUFFS);
 
+  function handleAddStuff(name, price) {
+    const newStuff = { name, price };
+    setStuffs([...stuffs, newStuff]);
+  }
   return (
     <>
       <header>
         <h1>My Stuff</h1>
       </header>
-
-      <StuffForm></StuffForm>
+      <StuffForm onAddStuff={handleAddStuff}/>
 
       <div className="stuff-list">
         {stuffs.map((stuff, index) => (
--- exercice 2/src/components/StuffForm.jsx
+++ exercice 2/src/components/StuffForm.jsx
@@ -1,12 +1,33 @@
-export default function StuffForm() {
+import { useState } from "react";
+
+export default function StuffForm({onAddStuff}) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [error, setError] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (name.trim() === "" || price.trim() === "" || Number(price) <= 0) {
+      setError("Please enter both name and price.!");
+      return;
+    }
+    setError("");
+
+    onAddStuff(name, price);
+    setName("");
+    setPrice("");
+
+    console.log(`A new object name ${name}, price ${price}$ will be added`);
+  }
   return (
-    <form className="stuff-form">
+    <form className="stuff-form" onSubmit={handleSubmit}>
       <p>Stuff name</p>
-      <input type="search" placeholder="Banana" />
+      <input type="search" placeholder="Banana" value={name} onChange={(e) => setName(e.target.value)}/>
 
       <p>Stuff price</p>
-      <input type="search" placeholder="15" />
+      <input type="search" placeholder="15" value={price} onChange={(e) => setPrice(e.target.value)}/>
 
+      {error && <p style={{color: "red"}}>{error}</p>}
       <button>Add Stuff</button>
     </form>
   );
