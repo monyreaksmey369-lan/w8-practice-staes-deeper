67c42c43e7650a6cf95c9a61408414af24878055
John <Monyreaksmey.sorm@student.cadt.edu.kh>
2026-03-15 15:56:58 +0700

    Apply gitignore

--- exercice 1/src/App.jsx
+++ exercice 1/src/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
+  const [score, setScore] = useState(0);
+  const handleChange = (event) => {
+    setScore(event.target.value);
+  }
   const getScoreBarStyle = () => {
     // 1- Compute width
-    const scoreWidth = `40%`;
+    const scoreWidth = `${score * 10}%`;
 
     // 2- Compute color (optional)
     let scoreColor = `#f3bc47`;
-
+    if (score <= 4) {
+      scoreColor = `#f3bc47`;
+    } else if (score <= 7) {
+      scoreColor = `#4ca340`;
+    } else if (score <= 9) {
+      scoreColor = `#69ff55`;
+    }else {
+      scoreColor = `#2a7c1f`;
+    }
     // 3 - Return the style object
     return {
       width: scoreWidth,
@@ -21,7 +33,7 @@ export default function App() {
         <h1>My Score in React</h1>
 
         <small>Enter a score (0 to 10): </small>
-        <input type="number" min="0" max="10"></input>
+        <input type="number" min="0" max="10" value={score} onChange={handleChange}></input>
 
         <div className="score-bar">
           <div className="score-bar-value" style={getScoreBarStyle()}></div>
